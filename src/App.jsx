e4106c51451e48e77f571756e8eaebfc2ef62ba8
Muaz Muhammad <muazmuhammad21@gmail.com>
2024-04-28 22:57:41 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Blogs from './component/blogs/Blogs'
+import Home from './component/home/Home'
+function App() {
+
+  return (
+   <div>
+      <Home></Home>
+      <Blogs></Blogs>
+   </div>
+  )
+}
+
+export default App
---
+++ src/component/home/Home.jsx
@@ -0,0 +1,12 @@
+
+import'./Home.css'
+
+
+function Home(){
+    return(
+        <div>
+            <h1 className='heading'>I'm from Home commponent</h1>
+        </div>
+    )
+}
+export default Home
