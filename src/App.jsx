a1a211ccfc315f9c4dffb677e781c244d61f2eff
Muaz Muhammad <muazmuhammad21@gmail.com>
2024-09-13 21:52:06 +0600

    update 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,18 @@
 
-import { useEffect , useState } from 'react'
 import './App.css'
-import Navigation from './components/Navigation/Navigation';
+import Navbar from './components/Navbar/Navbar'
+
 
 
 function App() {
    
-  const [allData, setAllData] = useState([]);
 
-
-  useEffect( () => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data => setAllData((data)))
-  }, [])
     
   return (
    <div>
-    <Navigation></Navigation>
-     <h1> Data: {allData.length} </h1>
-     {
-      allData.map(singleData => <p> {singleData.name} </p>)
-     }
-    
-
-     
+    <h1>Hello guys</h1>
+    <Navbar></Navbar>
+  
    </div>
   )
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-amber-400'>
+        <a href=""> Home </a>
+        <a href=""> Products </a>
+        <a href=""> Blogs </a>
+        <a href=""> About </a>
+        <a href=""> Contact Us </a>
+    </div>
+  )
+}
+
+export default Navbar
