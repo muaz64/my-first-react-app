dc85348a938eeb4d8d22ed79a82789540d70a99d
Muaz Muhammad <muazmuhammad21@gmail.com>
2024-09-13 22:32:44 +0600

    update 3

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='bg-amber-400'>
-        <a href=""> Home </a>
-        <a href=""> Products </a>
-        <a href=""> Blogs </a>
-        <a href=""> About </a>
-        <a href=""> Contact Us </a>
+        <Link> Home </Link>
+        <Link>Products </Link>
+        <Link> Blogs </Link>
+        <Link> About </Link>
+        <Link> Contact Us </Link>
+        
+    
     </div>
   )
 }
